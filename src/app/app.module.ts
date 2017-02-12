628f66a794b467240e41e1e8b99bf528468fc342
zideajang <zidea2015@163.com>
2017-02-12 16:13:35 +0800

    添加导航栏

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecommendComponent } from './pagers/recommend/recommend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecommendComponent
   ],
   imports: [
     BrowserModule,
