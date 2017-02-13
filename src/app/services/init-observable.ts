b43c33799de6419ed5944e8b070e27d70b596276
zideajang <zidea2015@163.com>
2017-02-13 16:41:05 +0800

    创建 subscribe

--- src/app/services/init-observable.ts
+++ src/app/services/init-observable.ts
@@ -1,10 +1,38 @@
 import { Observable } from 'rxjs/Rx';
+declare function fetch(url:string);
 
 export function initObservable(){
-    var keys$  = Observable.fromEvent(document,'keyup')
-                .do((keyUp:KeyboardEvent)=>console.log(keyUp.key))
-    keys$.subscribe();
-    // var key$ = ObservableInput.fromEvent(document,'keyup')
-    //             .do((keyUp:KeyboardEvent)=>console.log(keyUp.key));
-    // ObservableInput.fromEvent(document,'keyup')
+
+    // --------- lesson one ------------
+
+    // var keys$  = Observable.fromEvent(document,'keyup')
+    //             .do((keyUp:KeyboardEvent)=>console.log(keyUp.key))
+    // keys$.subscribe();
+
+    //----------- lesson two -----------
+
+    // fetch('https://api.github.com/users/zideajang/repos')
+    //     .then(res=>res.json())
+    //     .then(repo=>console.log(repo));
+    const reposUrl = 'https://api.github.com/users/zideajang/repos'
+    const reposPromise = fetch(reposUrl)
+                            .then(res=>res.json());
+    const repos$ = Observable.fromPromise(reposPromise)
+                            // .do(repos=>console.log(repos));
+                            // .map((key,index)=>{
+                            //     console.log(key," => ",index);
+                            // })
+
+    repos$.subscribe(
+        repos => console.log("new lessons", repos),
+
+        error => console.error("error",error),
+
+        ()=>console.log("done")
+    );
+
+
+
+
+
 }
