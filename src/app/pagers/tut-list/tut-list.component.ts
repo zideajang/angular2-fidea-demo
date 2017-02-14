3ef1bf26f263dcd5ebc310ea6d3416e0fcbea5d7
zideajang <zidea2015@163.com>
2017-02-14 16:51:44 +0800

    在组件中使用 http 获取服务器上数据

--- src/app/pagers/tut-list/tut-list.component.ts
+++ src/app/pagers/tut-list/tut-list.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Headers, Http, Response } from '@angular/http';
+import { Observable} from 'rxjs/Rx';
 @Component({
   selector: 'app-tut-list',
   templateUrl: './tut-list.component.html'
 })
 export class TutListComponent implements OnInit {
 
-  constructor() { }
+  constructor(http:Http) {
+
+    // const headers = new Headers();
+    // headers.append('Content-Type', 'application/json');
+    // headers.append('Content-Type', 'Access-Control-Allow-Headers');
+    
+    const response$:Observable<Response> = http.get('http://localhost:8000/tuts')
+                .map( (res:Response) => res.json() );
+    response$.subscribe(
+      res=>console.log(res),
+      ()=>{},
+      ()=>console.log("completed!")
+    );
+   }
 
   ngOnInit() {
   }
